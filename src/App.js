fd7fb21a082b8e6dd95d352f9e50c97cb33e3e7b
ovmkas <ovmkas@gmail.com>
2023-05-09 12:13:16 +0900

    p201

--- src/App.js
+++ src/App.js
@@ -6,9 +6,34 @@
 
 // export default App;
 
-import Info from "./Info";
+// import Info from "./Info";
+// function App() {
+//   return <Info />;
+// }
+
+// export default App;
+
+// import { useState } from "react";
+// import Info from "./Info.js";
+
+// function App() {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button onClick={() => setVisible(!visible)}>
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       {visible && <Info />}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Counter from "./Counter";
+
 function App() {
-  return <Info />;
+  return <Counter />;
 }
 
 export default App;
